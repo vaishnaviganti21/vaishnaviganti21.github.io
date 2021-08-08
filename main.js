8a041f35615cd3dd8d5b621f1f33516e0332ce55
Vaishnavi Ganti <vaishnaviganti21@gmail.com>
2021-08-07 22:08:12 -0400

    java

--- main.js
+++ main.js
@@ -8,3 +8,19 @@ function fibonacci(n){
     arr[i]=arr[i-1]+arr[i-2];
   }
 }
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  </head>
+  <body>
+    <div class="w3-container">
+      <p id="first-ptag">Hello, World!</p>
+      <p><input type="number" id="user-input"></input></p>
+      <p><button class="w3-button w3-black" onclick="askForName()">Hello</button></p>
+      <p><a href="index.html">Go Home</a></p>
+    </div>
+    <script src="main.js"></script>
+  </body>
+</html>
